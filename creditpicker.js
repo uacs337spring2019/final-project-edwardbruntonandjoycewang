9e1a24b975643d637b2164a225c893799191101e
Edward Brunton <edbrunton@email.arizona.edu>
2019-03-17 21:41:28 -0700

    added required packages

---
+++ creditpicker.js
@@ -0,0 +1,100 @@
+/*
+Edward Brunton
+CSC 337 001
+*/
+"use strict";
+(function () {
+    /**
+     * Enables the click buttons after the html loads
+     */
+    window.onload = function () {
+        document.getElementById("login").onclick = login;
+        document.getElementById("createNewAccount").onclick = newAccount;
+        document.getElementById("newAccount").onclick = switchToNewAccount;
+        document.getElementById("existingAccount").onclick = switchToExistingAccount;
+        document.getElementById("newAccountFieldSet").style.display = 'none';
+        console.log("loaded");
+    };
+    function switchToNewAccount() {
+        console.log("Switch to new account");
+        document.getElementById("existingAccountFieldSet").style.display = 'none';
+        document.getElementById("newAccountFieldSet").style.display = 'block';
+    }
+    function switchToExistingAccount() {
+        console.log("Switch to existing account");
+        document.getElementById("existingAccountFieldSet").style.display = 'block';
+        document.getElementById("newAccountFieldSet").style.display = 'none';
+    }
+    function login() {
+
+        console.log("Login button click");
+    }
+    function clearError() {
+        document.getElementById("error").innerHTML = "";
+    }
+    function addError(error) {
+        let errorDiv = document.createElement("div");
+        errorDiv.innerHTML = error;
+        document.getElementById("error").appendChild(errorDiv);
+    }
+    function newAccount() {
+        clearError();
+        let password = document.getElementById("passwordAccount").value;
+        let cPassword = document.getElementById("confirmPasswordAccount").value;
+        let name = document.getElementById("fullNameAccount").value.trim();
+        let email = document.getElementById("emailAccount").value;
+        let validItems = true;
+        if (password !== cPassword) {
+            addError("Passwords must match");
+            validItems = false;
+        }
+        if (password.length < 8) {
+            addError("Password must be at least 8 characters long");
+            validItems = false;
+        }
+        if (password.toUpperCase() === password || password.toLowerCase() === password) {
+            addError("Password must contain a mix of upper and lower case");
+            validItems = false;
+        }
+        if (password.indexOf(" ") !== -1) {
+            addError("Password must not have a space");
+            validItems = false;
+        }
+        let a = parseInt(password, 10);
+        console.log(a);
+        if (!/\d/.test(password)) {
+            addError("Password must contain a number");
+            validItems = false;
+        }
+        if (name.indexOf(" ") === -1 || name.length < 3) {
+            addError("Must have a first and a last name");
+            validItems = false;
+        }
+        if (!
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            addError("Email must have proper form");
+            validItems = false;
+        }
+        if (validItems) {
+            let url = "http://localhost:3000/";
+            const message = {
+                email: email,
+                fillName: name,
+                password: password
+            };
+            const fetchOptions = {
+                method: 'POST',
+                headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+                body: JSON.stringify(message)
+            };
+            fetch(url, fetchOptions)
+                .then(function (responseText) {
+                    loadFunction();
+                })
+                .catch(function (responseText){
+                    clearError();
+                    addError(response.statusText);
+                });
+        }
+    }
+})();
---
+++ creditpicker_service.js
@@ -0,0 +1,63 @@
+/*
+Edward Brunton
+CSC 337 001
+*/
+"use strict";
+const express = require("express");
+const app = express();
+const expressPort = 3000;
+let fs = require('fs');
+let mysql = require('mysql');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const bcrypt = require("bcrypt-nodejs");
+/**
+ * Allows cross origin calls
+ */
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+
+});
+/* Register a new user */
+app.post('/register', function (req, res, next) {
+    // Create a hash for the submitted password
+    console.log("Register request received");
+    
+    bcrypt.hash(req.body.password, null, null, function (err, hash) {
+        // Prepare a new user
+        console.log("Password successfully hashed");
+        
+    });
+});
+app.post('/', jsonParser, function (req, res) {
+    console.log("posting");
+  //  console.log(req);
+    const name = req.body.name;
+    const comment = req.body.comment;
+    console.log(name);
+    console.log(comment);
+    if(name.indexOf(":::") != -1 || comment.indexOf(":::") != -1
+    ||name.indexOf("\n") != -1 || comment.indexOf("\n") != -1){
+        console.log("invalid name or comment");
+        res.sendStatus(400);
+        return;
+    }
+    try{
+        fs.appendFileSync("messages.txt",  "\n" + name + ":::" + comment);
+    }catch{
+        console.log("could not write file");
+        res.sendStatus(500);
+        return;
+    }
+    console.log("Write success");
+    res.sendStatus(200);
+});
+app.listen(expressPort);
